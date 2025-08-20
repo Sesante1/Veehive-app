ef8e47b0a68ed0133d2f906ab7e65c8d95d4d922
Sesante1 <jomelsesante989@gmail.com>
2025-08-20 23:02:18 +0800

    Secure FirebaseConfig

---
+++ FirebaseConfig.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { initializeAuth, getReactNativePersistence } from "firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const firebaseConfig = {
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
+};
+
+// Initialize Firebase App
+export const FIREBASE_APP = initializeApp(firebaseConfig);
+
+// Firestore instance
+export const db = getFirestore(FIREBASE_APP);
+
+// Auth instance with React Native persistence
+export const FIREBASE_AUTH = initializeAuth(FIREBASE_APP, {
+  persistence: getReactNativePersistence(AsyncStorage),
+});
